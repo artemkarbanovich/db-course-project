98228c00b06bbd7715ce5ebe7038a9a47ab1e32d
artemkarbanovich <artyomkarbanovich@gmail.com>
2022-05-02 16:24:37 +0300

    web client: some fixes

--- webClient.Foodie/src/app/models/order-dish.ts
+++ webClient.Foodie/src/app/models/order-dish.ts
@@ -1,7 +1,9 @@
+import { Time } from "@angular/common";
+
 export interface OrderDish {
     dishesCount: number;
     name: string;
-    cookingTime: Date;
+    cookingTime: Time;
     dishWeight: number;
     price: number;
     ingredients: string;
