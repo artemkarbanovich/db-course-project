fb2ac17ff7c20379fa92508e8caf697eb86a51d0
artemkarbanovich <artyomkarbanovich@gmail.com>
2022-05-04 11:39:30 +0300

    init android client

---
+++ androidClient.Foodie/app/src/main/java/karbanovich/fit/bstu/foodie/MainActivity.java
@@ -0,0 +1,14 @@
+package karbanovich.fit.bstu.foodie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
